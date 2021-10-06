535734499421cbb6d4688baacab4d0f90e39ea90
umara <ua9404@rit.edu>
2021-10-06 08:45:51 +0400

    Screen dimensions

--- main.py
+++ main.py
@@ -2,13 +2,26 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import turtle
 
+XMAX = 200
+XMIN = -XMAX
+YMAX = 200
+YMIN = -YMAX
 
-def 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def init():
+    turtle.reset()
+    turtle.goto(XMIN, YMAX)
+
+    turtle.speed(0)
+    return
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
+    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
