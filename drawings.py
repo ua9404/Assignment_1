80ff35491979cff9981497e22edca5c20ebd3891
umara <ua9404@rit.edu>
2021-10-06 16:43:50 +0400

    run_all_drawings()

--- drawings.py
+++ drawings.py
@@ -15,7 +15,36 @@ def draw_shape_rectangle():
     turtle.right(90)
     turtle.forward(100)
     turtle.end_fill()
+    turtle.up()
+    turtle.home()
+
+def draw_shape_circle():
+    turtle.up()
+    turtle.goto(-100, -100)
+    turtle.down()
+    turtle.fillcolor("green")
+    turtle.begin_fill()
+    turtle.circle(50)
+    turtle.end_fill()
+    turtle.up()
+    turtle.home()
+
+def draw_shape_triangle():
+    turtle.up()
+    turtle.goto(50, 100)
+    turtle.down()
+    turtle.fillcolor("blue")
+    turtle.begin_fill()
+    turtle.right(60)
+    turtle.forward(80)
+    turtle.right(120)
+    turtle.forward(80)
+    turtle.right(120)
+    turtle.forward(80)
+    turtle.end_fill()
     turtle.done()
 
 
-
+draw_shape_rectangle()
+draw_shape_circle()
+draw_shape_triangle()
